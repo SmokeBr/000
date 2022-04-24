d146fd479ce796a557efd4237ac25e6004da108d
CeZaR <35664865+SmokeBr@users.noreply.github.com>
2022-04-23 21:09:25 -0300

    Add files via upload

--- noticias.js
+++ noticias.js
@@ -1,23 +1,20 @@
-var http = require('http');//biblioteca
+module.exports = function(app) {
 
-var server = http.createServer( function (req, res) {
+    app.get('/noticias', function(req, res) {
 
-    var categoria = req.url;
-    
-    if (categoria == '/tecnologia') {
-        res.end("<h1>Notícias de Tecnologia </h1>");
+        var mysql = require('mysql');
 
-    } else if (categoria == '/moda'){
-        res.end("<h1>Notícias de Moda </h1>");
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : 'root',
+            database : 'portal_noticias'
+        });
 
-    } else if (categoria == '/beleza') {
-        res.end("<h1>Notícias de Beleza </h1>");
-
-    } else {
-        res.end("<h1>Portal de Notícias !!!</h1>");
-
-    }
-
-}).listen(3000);
-
-console.log('ola');
+        connection.query('select * from noticias', function (error, result){
+            res.send(result);
+        });
+        
+        //res.render("noticias/noticias");
+    });
+};
